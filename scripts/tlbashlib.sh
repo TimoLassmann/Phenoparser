b278def3e21d12c9a60851ce83a324216f13e358
Richard Francis <richardwfrancis@gmail.com>
2018-02-09 20:02:02 +0800

    added echo_success and echo_failure

--- scripts/tlbashlib.sh
+++ scripts/tlbashlib.sh
@@ -1,7 +1,40 @@
+#!/bin/bash
 
-export PATH=$PATH:~/gemini/anaconda/bin
-export PATH=$PATH:~/bin
-. /etc/init.d/functions
+# thanks to http://tech.franzone.blog/2008/08/25/bash-script-init-style-status-message/
+# Column number to place the status message
+RES_COL=60
+# Command to move out to the configured column number
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+# Command to set the color to SUCCESS (Green)
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+# Command to set the color to FAILED (Red)
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+# Command to set the color back to normal
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
+# Function to print the SUCCESS status
+echo_success() {
+  $MOVE_TO_COL
+  echo -n "["
+  $SETCOLOR_SUCCESS
+  echo -n $"  OK  "
+  $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 0
+}
+
+# Function to print the FAILED status message
+echo_failure() {
+  $MOVE_TO_COL
+  echo -n "["
+  $SETCOLOR_FAILURE
+  echo -n $"FAILED"
+  $SETCOLOR_NORMAL
+  echo -n "]"
+  echo -ne "\r"
+  return 1
+}
 
 step() {
     echo -n "STEP: $@"
@@ -51,8 +84,7 @@ next() {
 }
 
 
-function file_exists ()
-{
+file_exists () {
     if ! [ "$1" ]
     then
         echo "file exists function needs an input";
